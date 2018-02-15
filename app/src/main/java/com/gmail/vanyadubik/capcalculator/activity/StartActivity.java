e0172bf8b1c111c03a5d34c3f11f95888d2ba141
Dubik Ivan <vanyadubik@gmail.com>
2018-02-15 10:56:51 +0200

    implement ui calc tax

--- app/src/main/java/com/gmail/vanyadubik/capcalculator/activity/StartActivity.java
+++ app/src/main/java/com/gmail/vanyadubik/capcalculator/activity/StartActivity.java
@@ -13,25 +13,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.gmail.vanyadubik.capcalculator.R;
 import com.gmail.vanyadubik.capcalculator.fragment.CalcMoneyFragment;
 import com.gmail.vanyadubik.capcalculator.fragment.CalcTaxFragment;
 import com.gmail.vanyadubik.capcalculator.utils.ActivityUtils;
+import com.michael.easydialog.EasyDialog;
 
 public class StartActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private boolean doubleBackToExitPressedOnce = false;
 
+    private Toolbar toolbar;
     private FragmentManager mFragmentManager;
+    private FragmentTransaction mFragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         mFragmentManager = getSupportFragmentManager();
@@ -45,7 +50,11 @@ public class StartActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
         navigationView.getMenu().getItem(0).setChecked(true);
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.containerView, new CalcMoneyFragment()).commit();
+        getSupportActionBar().setSubtitle(R.string.action_calc_money);
     }
 
     @Override
@@ -82,6 +91,10 @@ public class StartActivity extends AppCompatActivity
 
         int id = item.getItemId();
 
+        if(id == R.id.action_help){
+            showBubbleMessage();
+            return true;
+        }
         if (id == R.id.action_save) {
             return true;
         }
@@ -95,19 +108,19 @@ public class StartActivity extends AppCompatActivity
 
         int id = item.getItemId();
 
-        FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
-
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        
         if (id == R.id.nav_calc_money) {
-            xfragmentTransaction.replace(R.id.containerView, new CalcMoneyFragment()).commit();
+            mFragmentTransaction.replace(R.id.containerView, new CalcMoneyFragment()).commit();
             getSupportActionBar().setSubtitle(R.string.action_calc_money);
         } else if (id == R.id.nav_calc_tax) {
-            xfragmentTransaction.replace(R.id.containerView, new CalcTaxFragment()).commit();
+            mFragmentTransaction.replace(R.id.containerView, new CalcTaxFragment()).commit();
             getSupportActionBar().setSubtitle(R.string.action_calc_tax);
         } else if (id == R.id.nav_setting) {
             startActivity(new Intent(StartActivity.this, SettingsActivity.class));
         } else if (id == R.id.nav_exit) {
-            ActivityUtils.showQuestion(StartActivity.this, getString(R.string.questions_exit),
-                    getString(R.string.questions_data_set_default),
+            ActivityUtils.showQuestion(StartActivity.this, getString(R.string.action_exit),
+                    getString(R.string.questions_exit),
                     new ActivityUtils.QuestionAnswer() {
                         @Override
                         public void onPositiveAnsver() {
@@ -127,5 +140,33 @@ public class StartActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void showBubbleMessage(){
+
+        View bubbleMessageView = this.getLayoutInflater().inflate(R.layout.layout_tip_content_horizontal, null);
+        final EasyDialog bubbleMessage = new EasyDialog(StartActivity.this)
+                .setLayout(bubbleMessageView)
+                .setBackgroundColor(StartActivity.this.getResources().getColor(R.color.colorPrimary))
+                .setLocationByAttachedView(toolbar)
+                .setGravity(EasyDialog.GRAVITY_BOTTOM)
+                .setAnimationTranslationShow(EasyDialog.DIRECTION_X, 1000, -600, 100, -50, 50, 0)
+                .setAnimationAlphaShow(1000, 0.3f, 1.0f)
+                .setAnimationTranslationDismiss(EasyDialog.DIRECTION_X, 500, -50, 800)
+                .setAnimationAlphaDismiss(500, 1.0f, 0.0f)
+                .setTouchOutsideDismiss(true)
+                .setMatchParent(true)
+                .setMarginLeftAndRight(25, 25)
+                .setOutsideColor(StartActivity.this.getResources().getColor(R.color.outside_color_dark_gray));
+
+        ImageButton closeMessage = (ImageButton) bubbleMessageView.findViewById(R.id.closeMessage);
+        closeMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bubbleMessage.dismiss();
+            }
+        });
+
+        bubbleMessage.show();
     }
 }
--- app/src/main/java/com/gmail/vanyadubik/capcalculator/fragment/CalcTaxFragment.java
+++ app/src/main/java/com/gmail/vanyadubik/capcalculator/fragment/CalcTaxFragment.java
@@ -2,17 +2,32 @@ package com.gmail.vanyadubik.capcalculator.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.gmail.vanyadubik.capcalculator.R;
+import com.gmail.vanyadubik.capcalculator.model.MockData;
+import com.gmail.vanyadubik.capcalculator.utils.ActivityUtils;
+
+import java.util.List;
 
 public class CalcTaxFragment extends Fragment{
     private static  final int LAYOUT = R.layout.fragment_tax_paiment;
 
     private View view;
+    private EditText taxSystem, group, tax, incomeAll, incomeWithoutTax,
+            costsAll, costsWithoutTax;
+    private TextInputLayout groupTIL, taxTIL, incomeWithoutTaxTIL, incomeAllTIL,
+            costsAllTIL, costsWithoutTaxTIL;
+    private CardView costsCard;
+
+    private List<String> listTaxSystem, listGroup, listTax;
+    private int selectedtaxSystem, selectedGroup, selectedTax;
 
     public static CalcTaxFragment getInstance() {
 
@@ -27,7 +42,166 @@ public class CalcTaxFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
 
+        listTaxSystem = MockData.getListTaxSystem(getActivity());
+        listGroup = MockData.getListGroup(getActivity());
+        listTax = MockData.getListTax(getActivity());
+
+        taxSystem = (EditText) view.findViewById(R.id.tax_system);
+        groupTIL = (TextInputLayout) view.findViewById(R.id.group_til);
+        group = (EditText) view.findViewById(R.id.group);
+        taxTIL = (TextInputLayout) view.findViewById(R.id.tax_til);
+        tax = (EditText) view.findViewById(R.id.tax);
+        incomeAllTIL = (TextInputLayout) view.findViewById(R.id.income_all_til);
+        incomeAll = (EditText) view.findViewById(R.id.income_all);
+        incomeWithoutTaxTIL = (TextInputLayout) view.findViewById(R.id.income_without_tax_til);
+        incomeWithoutTax = (EditText) view.findViewById(R.id.income_without_tax);
+        costsCard = (CardView) view.findViewById(R.id.costs_card);
+        costsAllTIL = (TextInputLayout) view.findViewById(R.id.costs_til);
+        costsAll = (EditText) view.findViewById(R.id.costs);
+        costsWithoutTaxTIL = (TextInputLayout) view.findViewById(R.id.costs_without_tax_til);
+        costsWithoutTax = (EditText) view.findViewById(R.id.costs_without_tax);
+
+        taxSystem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ActivityUtils.showSelectionList(getActivity(),
+                        getString(R.string.tax_system), listTaxSystem,
+                        new ActivityUtils.ListItemClick() {
+                            @Override
+                            public void onItemClik(int item, String text) {
+                                setSelectionTaxSystem(item);
+                            }
+                        });
+            }
+        });
+
+        group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ActivityUtils.showSelectionList(getActivity(),
+                        getString(R.string.group), listGroup,
+                        new ActivityUtils.ListItemClick() {
+                            @Override
+                            public void onItemClik(int item, String text) {
+                                setSelectionGroup(item);
+                            }
+                        });
+            }
+        });
+
+
+        tax.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ActivityUtils.showSelectionList(getActivity(),
+                        getString(R.string.tax), listTax,
+                        new ActivityUtils.ListItemClick() {
+                            @Override
+                            public void onItemClik(int item, String text) {
+                                setSelectionTax(item);
+                            }
+                        });
+            }
+        });
+
+        groupTIL.setVisibility(View.GONE);
+        taxTIL.setVisibility(View.GONE);
+        incomeWithoutTaxTIL.setVisibility(View.GONE);
+        costsCard.setVisibility(View.GONE);
+        costsWithoutTaxTIL.setVisibility(View.GONE);
+
+        setSelectionTaxSystem(0);
+
+
         return view;
+    }
+
+
+    private void setSelectionTaxSystem(int item){
+        selectedtaxSystem = item;
+        taxSystem.setText(listTaxSystem.get(item));
+        incomeAll.setText("0.0");
+        costsAll.setText("0.0");
+        groupTIL.setVisibility(View.GONE);
+        taxTIL.setVisibility(View.GONE);
+        costsCard.setVisibility(View.GONE);
+        incomeWithoutTaxTIL.setVisibility(View.GONE);
+        costsWithoutTaxTIL.setVisibility(View.GONE);
+        switch (item) {
+            case 0:
+                groupTIL.setVisibility(View.VISIBLE);
+                setSelectionGroup(0);
+                return;
+            case 1:
+                taxTIL.setVisibility(View.VISIBLE);
+                setSelectionTax(0);
+                return;
+            case 2:
+                costsCard.setVisibility(View.VISIBLE);
+                return;
+            default:
+                return;
+        }
+    }
+
+    private void setSelectionGroup(int item){
+        selectedGroup= item;
+        group.setText(listGroup.get(item));
+        incomeWithoutTaxTIL.setVisibility(View.GONE);
+        costsWithoutTaxTIL.setVisibility(View.GONE);
+        taxTIL.setVisibility(View.GONE);
+        costsCard.setVisibility(View.GONE);
+        switch (item) {
+            case 0:
+                return;
+            case 1:
+                return;
+            case 2:
+                taxTIL.setVisibility(View.VISIBLE);
+                costsCard.setVisibility(View.VISIBLE);
+                setSelectionTax(0);
+                return;
+            default:
+                return;
+        }
+    }
+
+    private void setSelectionTax(int item){
+        selectedTax = item;
+        tax.setText(listTax.get(item));
+        incomeWithoutTax.setText("0.0");
+        costsWithoutTax.setText("0.0");
+        incomeWithoutTaxTIL.setVisibility(View.GONE);
+        switch (item) {
+            case 0:
+                incomeAllTIL.setHint(getActivity().getResources().getString(R.string.income_with_tax));
+                costsAllTIL.setHint(getActivity().getResources().getString(R.string.costs_with_tax));
+                incomeWithoutTaxTIL.setVisibility(View.VISIBLE);
+                if(selectedtaxSystem!=0) {
+                    costsWithoutTaxTIL.setVisibility(View.VISIBLE);
+                }
+                costsCard.setVisibility(View.VISIBLE);
+                if(selectedtaxSystem!=0) {
+                    costsAllTIL.setHint(getActivity().getResources().getString(R.string.costs_amount));
+                }
+                return;
+            case 1:
+                incomeAllTIL.setHint(getActivity().getResources().getString(R.string.income_amount));
+                costsAllTIL.setHint(getActivity().getResources().getString(R.string.costs_amount));
+                if(selectedtaxSystem!=0) {
+                    costsWithoutTaxTIL.setVisibility(View.GONE);
+                }else{
+                    costsCard.setVisibility(View.GONE);
+                }
+                return;
+            default:
+                return;
+        }
+
+
     }
 
 }
